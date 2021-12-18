cbb66c9b9ca442ef1c7a6fd1000145a3e0feacee
Wenqin Ye <wenqinye@Wenqins-MacBook-Pro.local>
2021-12-17 19:51:03 -0500

    save

--- src/diffuse.cpp
+++ src/diffuse.cpp
@@ -2,6 +2,13 @@
 #include <Eigen/Dense>
 #include <EigenTypes.h>
 
-void diffuse(Eigen::VectorXd &w1, Eigen::VectorXd &w0) {
+void diffuse(
+    Eigen::VectorXd &V_field_x1, Eigen::VectorXd &V_field_y1, Eigen::VectorXd &V_field_z1, // Output vector field
+    Eigen::VectorXd &V_field_x0, Eigen::VectorXd &V_field_y0, Eigen::VectorXd &V_field_z0, // Input vector field
+    double dt
+){
+    // Construct the gradient operator matrix
     
+
+    // Construct the divergence operator marix
 }
