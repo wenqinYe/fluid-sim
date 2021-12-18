2e82945b1113c5879a211e8cdec23a89afd2e6b1
benweisz <ben.weisz@mail.utoronto.ca>
2021-12-18 00:48:40 -0500

    Code simplifications

--- src/diffuse.cpp
+++ src/diffuse.cpp
@@ -15,6 +15,8 @@ void diffuse(
     global_field << V_field_x0, V_field_y0, V_field_z0;
 
     // Construct the gradient operator matrix
+    // ! TODO: This should probably be called the divergence and the other one the laplacian
+    // ! https://math.libretexts.org/Bookshelves/Calculus/Book%3A_Vector_Calculus_(Corral)/04%3A_Line_and_Surface_Integrals/4.06%3A_Gradient_Divergence_Curl_and_Laplacian
     Eigen::SparseMatrixd grad_operator(3 * dim3, 3 * dim3);
     typedef Eigen::Triplet<double> TRI;
     std::vector<TRI> tripletList_grad;
@@ -32,7 +34,7 @@ void diffuse(
                 tripletList_grad.push_back(TRI(row_ind, flat_index(i+1, j, k), pos_grad_coeff));
 
                 // Add y gradient opeartor for i, j, k
-                tripletList_grad.push_back(TRI(row_ind + dim, flat_index(i, j-1, k), neg_grad_coeff)); // ! TODO: the second parameter might have to be shifted
+                tripletList_grad.push_back(TRI(row_ind + dim, flat_index(i, j-1, k), neg_grad_coeff)); // ! TODO: the second parameter should be shifted
                 tripletList_grad.push_back(TRI(row_ind + dim, flat_index(i, j+1, k), pos_grad_coeff));
 
                 // Add z gradient operator for i, j, k
@@ -45,30 +47,26 @@ void diffuse(
 
 
     // Construct the divergence operator marix
+    // ! TODO: This is probably actually called the Laplacian and is computed differently (See above)
     Eigen::SparseMatrixd div_operator(3 * dim3, 3 * dim3);
-    std::vector<TRI> tripletList_div;
+    std::vector<TRI> tripletList_div;    
     for (int k = 1; k < dim - 1; k++) {
-
         for (int i = 1; i < dim - 1; i++) {
             for (int j = 1; j < dim - 1; j++) {
-                double delta_x = dim;
-                double delta_y = dim;
-                double delta_z = dim;
-
                 // Add divergence operator to x, y and z components
                 for (int component = 0; component < 3; component++) {
                     int row = flat_index(i, j, k) + component * dim3;
 
-                    tripletList_div.push_back(TRI(row, flat_index(i-1, j, k), -1.0 / (2.0 * delta_x)));
-                    tripletList_div.push_back(TRI(row, flat_index(i+1, j, k), 1.0 / (2.0 * delta_x)));
+                    // ! TODO: Im not entirly sure what happens here. It looks like its doing a first derivative but that doesnt seem to make sense
+                    tripletList_div.push_back(TRI(row, flat_index(i-1, j, k), neg_grad_coeff));
+                    tripletList_div.push_back(TRI(row, flat_index(i+1, j, k), pos_grad_coeff));
 
-                    tripletList_div.push_back(TRI(row, flat_index(i, j-1, k), -1.0 / (2.0 * delta_y)));
-                    tripletList_div.push_back(TRI(row, flat_index(i, j+1, k), 1.0 / (2.0 * delta_y)));
+                    tripletList_div.push_back(TRI(row, flat_index(i, j-1, k), neg_grad_coeff));
+                    tripletList_div.push_back(TRI(row, flat_index(i, j+1, k), pos_grad_coeff));
 
-                    tripletList_div.push_back(TRI(row, flat_index(i, j, k-1), -1.0 / (2.0 * delta_z)));
-                    tripletList_div.push_back(TRI(row, flat_index(i, j, k+1), 1.0 / (2.0 * delta_z)));
+                    tripletList_div.push_back(TRI(row, flat_index(i, j, k-1), neg_grad_coeff));
+                    tripletList_div.push_back(TRI(row, flat_index(i, j, k+1), pos_grad_coeff));
                 }
-
             }
         }
     }
@@ -77,11 +75,7 @@ void diffuse(
 
     // Construct sparse identity matrix
     Eigen::SparseMatrixd identity(3 * dim3, 3 * dim3);
-    std::vector<TRI> tripletList_identity;
-    for (int i = 0; i < dim3 * 3; i++) {
-        tripletList_identity.push_back(TRI(i, i, 1.0));
-    }
-    identity.setFromTriplets(tripletList_identity.begin(), tripletList_identity.end());
+    identity.setIdentity();
 
     // Create the diffusion operator and then solve for the new velocity field
     Eigen::SparseMatrixd A = identity - viscosity * dt * (div_operator * grad_operator);
